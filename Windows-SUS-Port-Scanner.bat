db02ab62ec409d408aec279bcf9e4ce4c52545ee
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2022-01-02 21:04:13 -0500

    Update Windows-SUS-Port-Scanner.bat

--- Windows-SUS-Port-Scanner.bat
+++ Windows-SUS-Port-Scanner.bat
@@ -25,8 +25,11 @@ if "!errorlevel!"=="0" (
         for /f "tokens=1,2 delims=:" %%B in ("%%~A") do (
             echo        IP: %%~B    Port: %%~C
             powershell "[console]::beep(3000,100)"
-            netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B/ / %%~C" dir=in action=block remoteip=%%~B/ / %%~C
-            netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B/ / %%~C" dir=out action=block remoteip=%%~B/ / %%~C
+            netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B" protocol=TCP dir=in remoteip=%%~B remoteport=%%~C action=block
+            netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B" protocol=TCP dir=out remoteip=%%~B remoteport=%%~C action=block
+			
+			netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B" protocol=TCP dir=in localip=%%~B localport=%%~C action=block
+            netsh advfirewall firewall add rule name="SUS CONNECTION: %%~B" protocol=TCP dir=out localip=%%~B localport=%%~C action=block
         )
     )
 ) else (
